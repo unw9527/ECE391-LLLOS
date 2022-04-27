b837fcded2123ffeec2f46d8c68d750cde3ed4eb
unw9527 <1041593558@qq.com>
2022-04-26 19:59:42 -0500

    update clear

--- syscalls/ece391clear.c
+++ syscalls/ece391clear.c
@@ -9,6 +9,10 @@ int main ()
     int32_t cnt;
     uint8_t buf[80*25];
     for (i = 0; i < 80*25-1; i++)
+        buf[i] = ' ';
+    buf[80*25-1] = '\0';
+    ece391_fdputs (1, buf);
+    for (i = 0; i < 80*25-1; i++)
         buf[i] = 0x08;
     buf[80*25-1] = '\0';
     ece391_fdputs (1, buf);
