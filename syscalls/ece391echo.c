8cb84d203a46a4f20aeb9f9deff7103a8b7441f5
unw9527 <1041593558@qq.com>
2022-04-26 19:29:21 -0500

    echo

--- syscalls/ece391echo.c
+++ syscalls/ece391echo.c
@@ -8,7 +8,8 @@ int main ()
     int32_t cnt;
     uint8_t buf[1024];
 
-    ece391_getargs (buf, 1024);
+    if (0 != ece391_getargs (buf, 1024))
+        return 0;
     cnt = ece391_read(1, buf, 1024);
     buf[cnt] = '\0';
     ece391_fdputs (1, buf);
